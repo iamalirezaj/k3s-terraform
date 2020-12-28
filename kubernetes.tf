400860a6ee1a166623f40b80a1afa8d976b43141
Alireza Josheghani <josheghani.dev@gmail.com>
2020-12-28 04:52:08 +0330

    Removing kubernetes and helm provider

--- kubernetes.tf
+++
@@ -1,8 +0,0 @@
-provider "kubernetes" {
-  load_config_file       = "false"
-  host                   = module.master.kubernetes.api_endpoint
-  client_certificate     = module.master.kubernetes.client_certificate
-  client_key             = module.master.kubernetes.client_key
-  cluster_ca_certificate = module.master.kubernetes.cluster_ca_certificate
-}
-
